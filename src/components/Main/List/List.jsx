5141f87fbb6f69ed0fa9cddfd9cd3ae8d7d5d054
nelllyai <worldpiece000@gmail.com>
2023-03-06 16:03:31 +0300

    add page argument

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,26 +1,53 @@
 // import style from './List.module.css';
 import Photo from './Photo';
 import {useDispatch, useSelector} from 'react-redux';
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 import {photosRequestAsync} from '../../../store/photos/photosAction';
 import {Masonry} from '@mui/lab';
 
 export const List = () => {
   const token = useSelector(state => state.token.token);
+
   const photos = useSelector(state => state.photos.data);
   const dispatch = useDispatch();
 
+  const endList = useRef(null);
+
   useEffect(() => {
-    dispatch(photosRequestAsync());
+    dispatch(photosRequestAsync(1));
   }, [token]);
 
-  return (
-    <Masonry columns={{xs: 2, sm: 3, md: 5}} spacing={1}>
-      {
-        photos.map(photo => (
-          <Photo key={photo.id} photoData={photo} />
-        ))
+  useEffect(() => {
+    if (!endList.current) return;
+
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        console.log('observe');
+        // dispatch(photosRequestAsync());
       }
-    </Masonry>
+    }, {
+      rootMargin: '100px',
+    });
+
+    observer.observe(endList.current);
+
+    return () => {
+      if (endList.current) {
+        observer.unobserve(endList.current);
+      }
+    };
+  }, [endList.current]);
+
+  return (
+    <>
+      <Masonry columns={{xs: 2, sm: 3, md: 5}} spacing={1}>
+        {
+          photos.map(photo => (
+            <Photo key={photo.id} photoData={photo} />
+          ))
+        }
+      </Masonry>
+      <div ref={endList} />
+    </>
   );
 };
--- src/store/photos/photosAction.js
+++ src/store/photos/photosAction.js
@@ -4,18 +4,19 @@ import {ACCESS_KEY, URL_API} from '../../api/const';
 
 export const photosRequestAsync = createAsyncThunk(
   'photos/get',
-  (args, {getState}) => {
+  (page, {getState}) => {
     const token = getState().token.token;
+    const prevPhotos = getState().photos.data;
 
     return axios(
-      `${URL_API}/photos?per_page=30`,
+      `${URL_API}/photos?page=${page}&per_page=30`,
       {
         headers: {
           Authorization: token ? `Bearer ${token}` : `Client-ID ${ACCESS_KEY}`,
         },
       })
       .then(({data}) => ({
-        data,
+        data: [...prevPhotos, ...data],
       }))
       .catch(error => Promise.reject(error));
   }
