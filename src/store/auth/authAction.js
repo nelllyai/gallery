1b3d1835448fa16fda3bc7c93aa454152b5d8ddb
nelllyai <worldpiece000@gmail.com>
2023-03-09 21:31:41 +0300

    realise deleting token after invalid authorization

--- src/store/auth/authAction.js
+++ src/store/auth/authAction.js
@@ -1,23 +1,22 @@
-/* eslint-disable arrow-body-style */
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import axios from 'axios';
 import {URL_API} from '../../api/const';
+import {deleteToken} from '../../utils/tokenStorage';
 
 export const authRequestAsync = createAsyncThunk(
   'auth/get',
-  (token, {getState}) => {
-    // const token = getState().token.token;
-
-    return axios(
-      `${URL_API}/me`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then(({data}) => ({
-        data,
-      }))
-      .catch(error => Promise.reject(error));
-  }
+  (token) => axios(
+    `${URL_API}/me`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(({data}) => ({
+      data,
+    }))
+    .catch(error => {
+      deleteToken();
+      return Promise.reject(error);
+    })
 );
